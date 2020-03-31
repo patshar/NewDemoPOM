ace3607486e22b7e8204c1410a069cedb9c751b3
Pratima <sharpat8@gmail.com>
2020-03-31 18:23:11 -0500

    Update login page

--- MavenPageObjectModelDemo/src/test/java/com/qa/test/LoginTest.java
+++ MavenPageObjectModelDemo/src/test/java/com/qa/test/LoginTest.java
@@ -1,11 +1,32 @@
 package com.qa.test;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
+	//com.qa.utils u=new com.qaWebDriverWait wait;.test.();
+	
+	com.qa.pages.HomePage h=new com.qa.pages.HomePage();
+	com.qa.pages.LoginPage l=new com.qa.pages.LoginPage();
+	com.qa.libs.TestWebPage w=new com.qa.libs.TestWebPage();
+	
+@BeforeTest
+public void browserPage() {
+	w.OpenWeb();
+	h.OpenBrowser();
+}
+@Test
+public void Test_1() {
+	System.out.println("Test 1");
+	l.Login();
+}
+	
+		
 	@Test
-	public void Test_1() {
-		System.out.println("Test 1");
+	public void Test_2() {
+		System.out.println("Test 2");
+		
 	}
+	
 }
